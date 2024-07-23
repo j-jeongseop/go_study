a7c162fde9f2bde516c6413c60faeadbe519b64f
happynet78 <smart2funding@gmail.com>
2024-07-23 20:49:41 +0900

    커밋

--- 05mymaths/main.go
+++ 05mymaths/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"crypto/rand"
-	"fmt"
-	"math/big"
+    "fmt"
+    "math/rand"
+    "time"
 )
 
 func main() {
-	fmt.Println("Welcome to maths in golang")
-
-	// var mynumberOne int = 2
-	// var mynumberTwo float64 = 4.5
-	//
-	// fmt.Println("The sum is: ", mynumberOne+int(mynumberTwo))
-
-	// random number
-	// rand.Seed(time.Now().UnixNano())
-	// fmt.Println(rand.Intn(5) + 1)
-
-	// random from crypto
-	myRandomNum, _ := rand.Int(rand.Reader, big.NewInt(5))
-	fmt.Println(myRandomNum)
+    fmt.Println("Welcome to maths in golang")
+    
+    // var mynumberOne int = 2
+    // var mynumberTwo float64 = 4.5
+    //
+    // fmt.Println("The sum is: ", mynumberOne+int(mynumberTwo))
+    
+    // random number
+    rand.Seed(time.Now().UnixNano())
+    fmt.Println(rand.Intn(5) + 1)
+    
+    // random from crypto
+    // myRandomNum, _ := rand.Int(rand.Reader, big.NewInt(5))
+    // fmt.Println(myRandomNum)
 }
--- 28channels/main.go
+++ 28channels/main.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sync"
+)
 
 func main() {
-	fmt.Println("Channels in golang-  LearnCodeOnline.in")
-
-	mych := make(chan int)
+    fmt.Println("Channels in golang-  LearnCodeOnline.in")
+    
+    myCh := make(chan int, 2)
+    wg := &sync.WaitGroup{}
+    
+    // fmt.Println(<-myCh)
+    // myCh <- 5
+    
+    wg.Add(2)
+    // R Only
+    go func(ch <-chan int, wg *sync.WaitGroup) {
+        val, isChanelOpen := <-myCh
+        
+        fmt.Println(isChanelOpen)
+        fmt.Println(val)
+        // fmt.Println(<-myCh)
+        
+        wg.Done()
+    }(myCh, wg)
+    
+    // send only
+    go func(ch chan<- int, wg *sync.WaitGroup) {
+        myCh <- 0
+        close(myCh)
+        // myCh <- 6
+        wg.Done()
+    }(myCh, wg)
+    
+    wg.Wait()
 }
