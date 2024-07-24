1d15c260f2d6b7c40e5c2809827bbb2c3779324d
happynet78 <smart2funding@gmail.com>
2024-07-24 21:52:10 +0900

    Final Commit

--- 05mymaths/main.go
+++ 05mymaths/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+    "crypto/rand"
     "fmt"
-    "math/rand"
-    "time"
+    "math/big"
 )
 
 func main() {
@@ -15,10 +15,10 @@ func main() {
     // fmt.Println("The sum is: ", mynumberOne+int(mynumberTwo))
     
     // random number
-    rand.Seed(time.Now().UnixNano())
-    fmt.Println(rand.Intn(5) + 1)
+    // rand.Seed(time.Now().UnixNano())
+    // fmt.Println(rand.Intn(5) + 1)
     
     // random from crypto
-    // myRandomNum, _ := rand.Int(rand.Reader, big.NewInt(5))
-    // fmt.Println(myRandomNum)
+    myRandomNum, _ := rand.Int(rand.Reader, big.NewInt(5))
+    fmt.Println(myRandomNum)
 }
